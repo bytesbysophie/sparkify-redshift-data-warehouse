75490c85df9cf8aa23a2dc0921cb0088dfe17dcb
Sophie Krüger <krueger.sophie@googlemail.com>
2020-07-05 19:02:10 +0200

    Adjust connection details

--- etl.py
+++ etl.py
@@ -2,7 +2,6 @@ import configparser
 import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
 
-
 def load_staging_tables(cur, conn):
     for query in copy_table_queries:
         cur.execute(query)
@@ -18,13 +17,22 @@ def insert_tables(cur, conn):
 def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
-
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
-    cur = conn.cursor()
     
+    DWH_ENDPOINT            = config.get("DWH","DWH_ENDPOINT")
+    DWH_DB                  = config.get("DWH","DWH_DB")
+    DWH_DB_USER             = config.get("DWH","DWH_DB_USER")
+    DWH_DB_PASSWORD         = config.get("DWH","DWH_DB_PASSWORD")
+    DWH_PORT                = config.get("DWH","DWH_PORT")
+
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(DWH_ENDPOINT, DWH_DB, DWH_DB_USER, DWH_DB_PASSWORD, DWH_PORT))
+    cur = conn.cursor()
+
+    print("Loading tables")
     load_staging_tables(cur, conn)
+    print("Creating tables")
     insert_tables(cur, conn)
 
+    print("Closing connection")
     conn.close()
 
 
