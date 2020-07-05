6c2f689c4361f95bfe27639d0f209fe855765e0f
Sophie Krüger <krueger.sophie@googlemail.com>
2020-07-05 19:41:10 +0200

    Add script to create the AWS redshift cluster and iam role

---
+++ create_cluster.py
@@ -0,0 +1,130 @@
+import boto3
+import json
+import time
+from botocore.exceptions import ClientError
+import configparser
+
+config = configparser.ConfigParser()
+config.read('dwh.cfg')
+
+KEY                    = config.get('AWS','KEY')
+SECRET                 = config.get('AWS','SECRET')
+
+DWH_CLUSTER_TYPE       = config.get("DWH","DWH_CLUSTER_TYPE")
+DWH_NUM_NODES          = config.get("DWH","DWH_NUM_NODES")
+DWH_NODE_TYPE          = config.get("DWH","DWH_NODE_TYPE")
+
+DWH_CLUSTER_IDENTIFIER = config.get("DWH","DWH_CLUSTER_IDENTIFIER")
+DWH_DB                 = config.get("DWH","DWH_DB")
+DWH_DB_USER            = config.get("DWH","DWH_DB_USER")
+DWH_DB_PASSWORD        = config.get("DWH","DWH_DB_PASSWORD")
+DWH_PORT               = config.get("DWH","DWH_PORT")
+
+DWH_IAM_ROLE_NAME      = config.get("DWH", "DWH_IAM_ROLE_NAME")
+
+
+def create_clients():
+
+    iam = boto3.client('iam',aws_access_key_id=KEY,
+                        aws_secret_access_key=SECRET,
+                        region_name='us-west-2'
+                    )
+
+    redshift = boto3.client('redshift',
+                        region_name="us-west-2",
+                        aws_access_key_id=KEY,
+                        aws_secret_access_key=SECRET
+                        )
+
+    ec2 = boto3.resource('ec2',
+                       region_name="us-east-1",
+                       aws_access_key_id=AWS_ACCESS_KEY_ID,
+                       aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+
+    print("Created Clients")
+    return iam, redshift, ec2
+
+
+def create_iam_role(iam):
+
+    try:
+        print("Creating a new IAM Role") 
+        dwhRole = iam.create_role(
+            Path='/',
+            RoleName=DWH_IAM_ROLE_NAME,
+            Description = "Allows Redshift clusters to call AWS services on your behalf.",
+            AssumeRolePolicyDocument=json.dumps(
+                {'Statement': [{'Action': 'sts:AssumeRole',
+                'Effect': 'Allow',
+                'Principal': {'Service': 'redshift.amazonaws.com'}}],
+                'Version': '2012-10-17'})
+        )    
+    except Exception as e:
+        print(e)
+        
+    print("Attaching Policy")
+    iam.attach_role_policy(RoleName=DWH_IAM_ROLE_NAME,
+                        PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+                        )['ResponseMetadata']['HTTPStatusCode']
+
+    return iam.get_role(RoleName=DWH_IAM_ROLE_NAME)['Role']['Arn']
+
+
+def create_redshift_cluster(iam, redshift, ec2):
+
+    try:
+        roleArn = create_iam_role(iam)
+        response = redshift.create_cluster(        
+            ClusterType=DWH_CLUSTER_TYPE,
+            NodeType=DWH_NODE_TYPE,
+            NumberOfNodes=int(DWH_NUM_NODES),
+            #Identifiers & Credentials
+            DBName=DWH_DB,
+            ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,
+            MasterUsername=DWH_DB_USER,
+            MasterUserPassword=DWH_DB_PASSWORD,
+            #Roles (for s3 access)
+            IamRoles=[roleArn]  
+        )
+
+    except Exception as e:
+        print(e)
+
+    myClusterProps = redshift.describe_clusters(ClusterIdentifier=DWH_CLUSTER_IDENTIFIER)['Clusters'][0]
+
+    # Wait until the cluster is available for further operations
+    while True:
+        response = redshift.describe_clusters(ClusterIdentifier=DWH_CLUSTER_IDENTIFIER)
+        cluster_info = response['Clusters'][0]
+        if cluster_info['ClusterStatus'] == 'available':
+            print("The cluster is ready")
+            break
+        time.sleep(10)
+
+    # Open incoming TCP port
+    try:
+        vpc = ec2.Vpc(id=myClusterProps['VpcId'])
+        defaultSg = list(vpc.security_groups.all())[0]
+        print(defaultSg)
+        defaultSg.authorize_ingress(
+            GroupName=defaultSg.group_name,
+            CidrIp='0.0.0.0/0',
+            IpProtocol='TCP',
+            FromPort=int(DWH_PORT),
+            ToPort=int(DWH_PORT)
+        )
+    except Exception as e:
+        print(e)
+
+
+    endpoint = myClusterProps['Endpoint']['Address']
+    print("cluster endpoint: {}".format(endpoint))
+
+
+def main():
+
+    iam, redshift, ec2 = create_clients()
+    create_redshift_cluster(iam, redshift, ec2)
+
+if __name__ == "__main__":
+    main()
