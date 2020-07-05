06866a8652e751a0faa0b19b24de4e7a526afeea
Sophie Krüger <krueger.sophie@googlemail.com>
2020-07-05 19:01:47 +0200

    Change timestamp data type to fix loading data into staging tables

--- create_tables.py
+++ create_tables.py
@@ -1,7 +1,7 @@
 import configparser
 import psycopg2
+import boto3
 from sql_queries import create_table_queries, drop_table_queries
-
 
 def drop_tables(cur, conn):
     for query in drop_table_queries:
@@ -16,15 +16,24 @@ def create_tables(cur, conn):
 
 
 def main():
+
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
+    DWH_ENDPOINT            = config.get("DWH","DWH_ENDPOINT")
+    DWH_DB                  = config.get("DWH","DWH_DB")
+    DWH_DB_USER             = config.get("DWH","DWH_DB_USER")
+    DWH_DB_PASSWORD         = config.get("DWH","DWH_DB_PASSWORD")
+    DWH_PORT                = config.get("DWH","DWH_PORT")
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(DWH_ENDPOINT, DWH_DB, DWH_DB_USER, DWH_DB_PASSWORD, DWH_PORT))
     cur = conn.cursor()
 
+    print("Dropping tables")
     drop_tables(cur, conn)
+    print("Creating tables")
     create_tables(cur, conn)
-
+    
+    print("Closing connection")
     conn.close()
 
 
