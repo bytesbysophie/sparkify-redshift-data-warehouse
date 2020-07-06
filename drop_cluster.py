2ca30304612607531ffb929d3d6e4c9f7cb4fa61
Sophie Krüger <krueger.sophie@googlemail.com>
2020-07-06 17:54:00 +0200

    Fix minor bugs and inconsistent names

--- drop_cluster.py
+++ drop_cluster.py
@@ -6,8 +6,8 @@ import configparser
 config = configparser.ConfigParser()
 config.read('dwh.cfg')
 
-AWS_ACCESS_KEY_ID      = config.get('AWS','KEY')
-AWS_SECRET_ACCESS_KEY  = config.get('AWS','SECRET')
+AWS_KEY      = config.get('AWS','AWS_KEY')
+AWS_SECRET  = config.get('AWS','AWS_SECRET')
 DWH_CLUSTER_IDENTIFIER = config.get("DWH","DWH_CLUSTER_IDENTIFIER")
 DWH_IAM_ROLE_NAME      = config.get('DWH', "DWH_IAM_ROLE_NAME")
 
@@ -15,30 +15,30 @@ def main():
 
     redshift = boto3.client('redshift',
                         region_name="us-west-2",
-                        aws_access_key_id=AWS_ACCESS_KEY_ID,
-                        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+                        aws_access_key_id=AWS_KEY,
+                        aws_secret_access_key=AWS_SECRET
                         )
-    iam = boto3.client('iam',aws_access_key_id=AWS_ACCESS_KEY_ID,
-                        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    iam = boto3.client('iam',aws_access_key_id=AWS_KEY,
+                        aws_secret_access_key=AWS_SECRET,
                         region_name='us-west-2'
                     )
 
+    # DELETE IAM ROLE
+    iam.detach_role_policy(RoleName=DWH_IAM_ROLE_NAME, PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess")
+    iam.delete_role(RoleName=DWH_IAM_ROLE_NAME)
+    
     # DELETE CLUSTER
     redshift.delete_cluster( ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,  SkipFinalClusterSnapshot=True)
     print("Deleting cluster {}".format(DWH_CLUSTER_IDENTIFIER))
     while True:
         try:
             response = redshift.describe_clusters(ClusterIdentifier=DWH_CLUSTER_IDENTIFIER)
-            cluster_info = response1['Clusters'][0]
+            cluster_info = response['Clusters'][0]
             time.sleep(10)
         except:
 
             print("Deleted!")
             break
-
-    # DELETE IAM ROLE
-    iam.detach_role_policy(RoleName=DWH_IAM_ROLE_NAME, PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess")
-    iam.delete_role(RoleName=DWH_IAM_ROLE_NAME)
 
 if __name__ == "__main__":
     main()
